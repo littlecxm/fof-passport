c84e6eef031f145b9f81280f3019f60046c8ea6e
cxm <littlecxm@gmail.com>
2024-08-20 11:35:19 +0800

    fix: modal

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,58 +1,58 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('littlecxm-fof-passport', function () {
-    app.extensionData
-        .for('littlecxm-fof-passport')
-        .registerSetting({
-            label: app.translator.trans('fof-passport.admin.popup.field.app-auth-url'),
-            setting: 'littlecxm-fof-passport.app_auth_url',
-            type: 'text',
-            placeholder: 'https://example.com/oauth/authorize',
-        })
-        .registerSetting({
-            label: app.translator.trans('fof-passport.admin.popup.field.app-token-url'),
-            setting: 'littlecxm-fof-passport.app_token_url',
-            type: 'text',
-            placeholder: 'https://example.com/oauth/token',
-        })
-        .registerSetting({
-            label: app.translator.trans('fof-passport.admin.popup.field.app-user-url'),
-            setting: 'littlecxm-fof-passport.app_user_url',
-            type: 'text',
-            placeholder: 'https://example.com/api/user',
-        })
-        .registerSetting({
-            label: app.translator.trans('fof-passport.admin.popup.field.app-id'),
-            setting: 'littlecxm-fof-passport.app_id',
-            type: 'text',
-            placeholder: '123',
-        })
-        .registerSetting({
-            label: app.translator.trans('fof-passport.admin.popup.field.app-secret'),
-            setting: 'littlecxm-fof-passport.app_secret',
-            type: 'text',
-            placeholder: 'abcdefghijABCDEFGHIJabcdefghijABCDEFGHIJ',
-        })
-        .registerSetting({
-            label: app.translator.trans('fof-passport.admin.popup.field.app-scopes'),
-            setting: 'littlecxm-fof-passport.app_oauth_scopes',
-            type: 'text',
-        })
-        .registerSetting({
-            label: app.translator.trans('fof-passport.admin.popup.field.button-title'),
-            setting: 'littlecxm-fof-passport.button_title',
-            type: 'text',
-            placeholder: app.translator.trans('fof-passport.admin.popup.field.button-title-placeholder'),
-        })
-        .registerSetting({
-            label: app.translator.trans('fof-passport.admin.popup.field.button-icon'),
-            setting: 'littlecxm-fof-passport.button_icon',
-            type: 'text',
-            placeholder: 'far fa-id-card',
-        })
-        .registerSetting({
-            setting: 'littlecxm-fof-passport.auto_reg',
-            type: 'boolean',
-            label: app.translator.trans('fof-passport.admin.popup.field.auto-reg'),
-        });
+  app.extensionData
+    .for('littlecxm-fof-passport')
+    .registerSetting({
+      label: app.translator.trans('fof-passport.admin.popup.field.app-auth-url'),
+      setting: 'littlecxm-fof-passport.app_auth_url',
+      type: 'text',
+      placeholder: 'https://example.com/oauth/authorize',
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-passport.admin.popup.field.app-token-url'),
+      setting: 'littlecxm-fof-passport.app_token_url',
+      type: 'text',
+      placeholder: 'https://example.com/oauth/token',
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-passport.admin.popup.field.app-user-url'),
+      setting: 'littlecxm-fof-passport.app_user_url',
+      type: 'text',
+      placeholder: 'https://example.com/api/user',
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-passport.admin.popup.field.app-id'),
+      setting: 'littlecxm-fof-passport.app_id',
+      type: 'text',
+      placeholder: '123',
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-passport.admin.popup.field.app-secret'),
+      setting: 'littlecxm-fof-passport.app_secret',
+      type: 'text',
+      placeholder: 'abcdefghijABCDEFGHIJabcdefghijABCDEFGHIJ',
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-passport.admin.popup.field.app-scopes'),
+      setting: 'littlecxm-fof-passport.app_oauth_scopes',
+      type: 'text',
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-passport.admin.popup.field.button-title'),
+      setting: 'littlecxm-fof-passport.button_title',
+      type: 'text',
+      placeholder: app.translator.trans('fof-passport.admin.popup.field.button-title-placeholder'),
+    })
+    .registerSetting({
+      label: app.translator.trans('fof-passport.admin.popup.field.button-icon'),
+      setting: 'littlecxm-fof-passport.button_icon',
+      type: 'text',
+      placeholder: 'far fa-id-card',
+    })
+    .registerSetting({
+      setting: 'littlecxm-fof-passport.auto_reg',
+      type: 'boolean',
+      label: app.translator.trans('fof-passport.admin.popup.field.auto-reg'),
+    });
 });
--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,4 +1,4 @@
-import { extend } from 'flarum/common/extend';
+import { extend, override } from 'flarum/common/extend';
 import app from 'flarum/forum/app';
 import LogInButtons from 'flarum/forum/components/LogInButtons';
 import LogInButton from 'flarum/forum/components/LogInButton';
@@ -7,24 +7,31 @@ import SettingsPage from 'flarum/forum/components/SettingsPage';
 import ItemList from 'flarum/common/utils/ItemList';
 
 app.initializers.add('fof-passport', () => {
-    extend(LogInButtons.prototype, 'items', function (items: ItemList) {
-        items.add(
-            'fof-passport',
-            LogInButton.component(
-                {
-                    className: 'Button LogInButton--passport',
-                    icon: app.forum.attribute('fof-passport.loginIcon'),
-                    path: '/auth/passport',
-                },
-                app.forum.attribute('fof-passport.loginTitle')
-            )
-        );
-    });
-    extend(LogInModal.prototype, 'fields', function (fields: any) {
-        fields.remove('identification')
-        fields.remove('password')
-    });
-    extend(SettingsPage.prototype, 'settingsItems', function (items: any) {
-        items.remove('account')
-    });
+  extend(LogInButtons.prototype, 'items', function (items: ItemList) {
+    items.add(
+      'fof-passport',
+      LogInButton.component(
+        {
+          className: 'Button LogInButton--passport',
+          icon: app.forum.attribute('fof-passport.loginIcon'),
+          path: '/auth/passport',
+        },
+        app.forum.attribute('fof-passport.loginTitle')
+      )
+    );
+  });
+  extend(LogInModal.prototype, 'fields', function (fields: any) {
+    fields.remove('identification');
+    fields.remove('password');
+    fields.remove('submit');
+  });
+  override(LogInModal.prototype, 'footer', function () {
+    return null;
+  });
+  override(LogInModal.prototype, 'forgotPassword', function () {
+    return null;
+  });
+  extend(SettingsPage.prototype, 'settingsItems', function (items: any) {
+    items.remove('account');
+  });
 });
